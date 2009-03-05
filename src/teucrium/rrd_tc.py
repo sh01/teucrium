ac330e4d6adf842100f0ff3bde7e28a67c0cbcea
Sebastian Hagen <sebastian_hagen@memespace.net>
2009-03-05 15:17:50 +0100

    Change rrd update method to pipe-controlled rrdtool child processes, allowing non-blocking data commits.

--- src/teucrium/rrd_tc.py
+++ src/teucrium/rrd_tc.py
@@ -15,30 +15,85 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import logging
 import os
 
-import rrdtool
 try:
    from gonium.linux.xtables import XTablesPoller
 except ImportError:
    pass
 
+from gonium.fd_management import CHILD_REACT_KILL
+
 from constants import CT_BYTES, CT_PACKETS
 from rrd_fn import RRDFileNamer
 
 class RRDTrafficCounter(RRDFileNamer):
-   def __init__(self, rrd_base_filename, rules2ds, chain2diriface, commit_interval):
+   logger = logging.getLogger('RRDTrafficCounter')
+   log = logger.log
+   def __init__(self, ed, rrd_base_filename, rules2ds, chain2diriface, commit_interval):
+      self.ed = ed
       self.rrd_base_filename = rrd_base_filename
       self.rules2ds = rules2ds
       self.chain2diriface = chain2diriface
       self.commit_interval = commit_interval
       self.commit_index = 0
       self.output_cache = {}
+      self.rrd_child = None
+      self.rrd_line_cache = []
+   
+   def rrd_child_spawn(self):
+      if not (self.rrd_child is None):
+         raise StandardError('I already have an active rrd_child: %r' % self.rrd_child)
+      self.rrd_child = self.ed.ChildRunnerPopen4(('rrdtool', '-'),
+         self.child_termination_process, finish=CHILD_REACT_KILL,
+         input_handler=self.child_input_process)
+   
+   def child_input_process(self, child, fd):
+      lines = child.buffers_input[fd].split('\n')
+      child.buffers_input[fd] = lines[-1]
+      del(lines[-1])
+      idx_start = 0
+      for i in range(len(lines)):
+         line = lines[i]
+         if (line.startswith('OK')):
+            if (self.rrd_line_cache):
+               del(self.rrd_line_cache[:])
+            idx_start = i+1
+            self.log(20, 'Sucessfully executed rrd command: %r' % (line,))
+            continue
+         if (line.startswith('ERROR')):
+            error_lines = self.rrd_line_cache + lines[idx_start:i+1]
+            self.log(38, 'rrdtool error: %r' % '\n'.join(error_lines))
+            idx_start = i+1
+            if (self.rrd_line_cache):
+               del(self.rrd_line_cache[:])
+            continue
+         if (line.startswith('For more information read the RRD manpages')):
+            self.log(40, 'Noticed rrdtool syntax error!')
+      
+      self.rrd_line_cache.extend(lines[idx_start:])
+   
+   def child_termination_process(self, child, return_code, exit_status):
+      self.log(26, '%r notes termination of rrdtool child process RC: %r ES:'
+         '%r output: %r.' % (self,return_code, exit_status))
+      self.rrd_child = None
+      self.rrd_line_cache = []
+   
+   def rrdfiles_update(self, fn, val_seq):
+      if (self.rrd_child is None):
+         self.rrd_child_spawn()
+      
+      # Using full python string escape sequences isn't *exactly* correct, but
+      # as long as people don't try to deliberately mess up their own setup,
+      # it shouldn't cause any problems.
+      self.rrd_child.send_data('update %r -t %r %s\n' % (fn, self.DS_RAW,
+         ' '.join('%s:%s' % (tss, val) for (tss,val) in val_seq)))
    
    @classmethod
    def build_with_xtp(cls, ed, interval, xt, tables, *args, **kwargs):
       xtp = XTablesPoller(ed, interval, xt, tables)
-      self = cls(*args, **kwargs)
+      self = cls(ed, *args, **kwargs)
       self.xtp = xtp
       xtp.em_xtentries.EventListener(self.xtp_data_process)
       # Write buffered data on shutdown
@@ -47,8 +102,7 @@ class RRDTrafficCounter(RRDFileNamer):
    def rrd_data_commit(self):
       for ((iface, dir_, ds, ct), val_list) in self.output_cache.items():
          target_fn = self.rrd_fn_get(iface, dir_, ct, ds)
-         rrdtool.update(target_fn, '-t', self.DS_RAW,
-            *('%s:%s' % (tss, val) for (tss,val) in val_list))
+         self.rrdfiles_update(target_fn, val_list)
          del(val_list[:])
    
    def rrd_data_queue(self, iface, dir_, ds_l, tss, cbytes_l, cpackets_l):
